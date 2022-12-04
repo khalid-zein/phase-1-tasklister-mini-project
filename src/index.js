c3fca669cff0c27c9f73ae5ee3a800c3572dea05
khalid-zein <zeinkhalid004@gmail.com>
2022-12-04 23:47:30 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const myForm = document.getElementById("create-task-form");
+
+
+  const todoItem = function (event) {
+    event.preventDefault();
+    const input = document.getElementById("new-task-description").value;
+    const todoList = document.getElementById("tasks");
+    const item = document.createElement("li");
+    item.textContent = input;
+
+
+    todoList.prepend(item);
+
+    
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "x";
+    item.append(deleteBtn);
+    const deleteTodo = function () {
+      item.remove()
+    };
+    deleteBtn.addEventListener("click", deleteTodo);
+  };
+
+  myForm.addEventListener("submit", todoItem);
 });
